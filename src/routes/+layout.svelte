80b88b029816769b783d2db03e9747c5ccc748db
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2026-01-06 22:28:01 -0800

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<h1>My Doryoku Log</h1>
+
+{@render children()}
