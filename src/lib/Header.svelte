77c20df6f2534c0a3edd19e9c7036f417fd0aa04
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2026-01-08 11:57:56 -0800

    header for app

---
+++ src/lib/Header.svelte
@@ -0,0 +1,44 @@
+<script>
+	export let label;
+</script>
+
+<style>
+	.header {
+			display: flex;
+			align-items: center;
+			overflow: hidden;
+      background-color: #e1e2e6;
+      padding: 16px;
+	}
+
+  .header h1 {
+      float: left;
+      color: #1c2127;
+      text-align: center;
+      padding: 6px;
+      text-decoration: none;
+      font-size: 24px;
+      line-height: 25px;
+      border-radius: 4px;
+  }
+
+	.header h1.logo {
+      font-size: 28px;
+      font-weight: bold;
+      display: flex;
+      align-items: center;
+  }
+
+  .header-logo img {
+      width: 40px;
+      height: 40px;
+			display: block;
+  }
+</style>
+
+<div class="header">
+	<h1 class="logo">{label}</h1>
+	<div class="header-logo" aria-hidden="true">
+		<img src="/src/lib/assets/notebook.svg" alt="" />
+	</div>
+</div>
