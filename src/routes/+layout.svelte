2154f0060d37940bbf3b7a0ff18525377ba37491
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2026-01-08 11:58:12 -0800

    Set header for all pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+	import Header from '$lib/Header.svelte';
 	let { children } = $props();
 </script>
 
 <svelte:head>
+	<title>My Doryoku Log</title>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<h1>My Doryoku Log</h1>
+<Header label="My Doryoku Log" />
 
 {@render children()}
