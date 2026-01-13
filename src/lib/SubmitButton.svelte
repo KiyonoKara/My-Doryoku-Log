f4556f2dfaa558a448db095c4fd8bc45f2d9f487
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2026-01-13 14:57:50 -0800

    (Breaking) feature: typed submission button

--- src/lib/SubmitButton.svelte
+++ src/lib/SubmitButton.svelte
@@ -1,9 +1,9 @@
-<script>
-	export let label = 'Submit';
+<script lang="ts">
+	export let label: string = 'Submit';
 	export let onClick = () => {};
 
-	let pressed = false;
-	let button;
+	let pressed: boolean = false;
+	let button: HTMLButtonElement;
 
 	function handleClick() {
 		pressed = true;
@@ -15,47 +15,48 @@
 	}
 </script>
 
-<button bind:this={button}
-				class="submit-button"
-				class:pressed
-				on:click={handleClick}
-				on:animationend={() => pressed = false}>{label}
+<button
+	bind:this={button}
+	class="submit-button"
+	class:pressed
+	on:click={handleClick}
+	on:animationend={() => (pressed = false)}
+	>{label}
 </button>
 
 <style>
- .submit-button {
-		 border: none;
-		 padding: 0.5em 1.0em;
-		 border-radius: 0.5em;
-		 color: white;
-     background-color: #3373b0;
-		 font-weight: bold;
-		 cursor: pointer;
-		 text-align: center;
-     display: inline-block;
-		 font-size: 1em;
-     transition:
-						 background-color 0.1s ease-in-out,
-             color 0.1s ease-in-out;
- }
+	.submit-button {
+		border: none;
+		padding: 0.5em 1em;
+		border-radius: 0.5em;
+		color: white;
+		background-color: #3373b0;
+		font-weight: bold;
+		cursor: pointer;
+		text-align: center;
+		display: inline-block;
+		font-size: 1em;
+		transition:
+			background-color 0.1s ease-in-out,
+			color 0.1s ease-in-out;
+	}
 
- .submit-button:hover {
-		 background-color: #0b385f;
-		 color: #bed4e9;
- }
+	.submit-button:hover {
+		background-color: #0b385f;
+		color: #bed4e9;
+	}
 
- .submit-button.pressed {
-		 /*background-color: #122335;*/
-		 background-color: #a2c0df;
-		 color: #1c2127;
- }
+	.submit-button.pressed {
+		/*background-color: #122335;*/
+		background-color: #a2c0df;
+		color: #1c2127;
+	}
 
- .submit-button:focus-visible {
-     background-color: #bed4e9;
-     color: #1c2127;
-		 transition:
-             background-color 5s ease-in-out,
-             color 5s ease-in-out;
- }
-
+	.submit-button:focus-visible {
+		background-color: #bed4e9;
+		color: #1c2127;
+		transition:
+			background-color 5s ease-in-out,
+			color 5s ease-in-out;
+	}
 </style>
