5e4a30603f44ec7c03fb2cb96daac2d83df49fa5
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2026-02-06 11:43:16 -0800

    feature: polish get transactions from local db function and write an adapter to turn it into json array

--- backend/finance_ml/db.py
+++ backend/finance_ml/db.py
@@ -1,9 +1,35 @@
 import sqlite3
+import os
 
-con = sqlite3.connect('../../local.db')
-cur = con.cursor()
+# get local.db from app root
+script_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(os.path.dirname(script_dir))
+DB_PATH = os.path.join(project_root, 'local.db')
 
-tr = cur.execute("SELECT * FROM transactions")
+# connect to db
+con = sqlite3.connect(DB_PATH, check_same_thread=False)
 
-def get_transactions():
-    return tr.fetchall()
+def get_transactions() -> list[tuple]:
+    """
+    Gets all transactions from database
+    :return: List of tuples of transactions
+    """
+    cur = con.cursor()
+    tr = cur.execute("SELECT * FROM transactions")
+    columns = tr.fetchall()
+    cur.close()
+    return columns
+
+def convert_transactions_to_dict(transactions) -> list[dict]:
+    """
+    Transactions from the SQLite transactions table
+    :param transactions:
+    :return: List of dictionaries
+    """
+    data = []
+    for transaction in transactions:
+        vals = {'id': transaction[0], 'date': transaction[1], 'amount': transaction[2],
+                'category': transaction[3], 'type': transaction[4],
+                'description': transaction[5]}
+        data.append(vals)
+    return data
