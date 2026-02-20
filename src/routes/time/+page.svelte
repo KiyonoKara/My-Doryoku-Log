d821dce1e40098a07c5d9eb89ab3e2a6cdadc5e5
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2026-02-19 23:58:03 -0800

    feature: start time tracking now

---
+++ src/routes/time/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import CsvExportButton from '$lib/buttons/CsvExportButton.svelte';
+	import { capitalizeFirstLetter } from '$lib/utils/util';
+	import '../../flash_notif.css';
+</script>
