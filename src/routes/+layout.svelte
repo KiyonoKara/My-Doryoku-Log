3e4094a390792274a8453a6a3731fdbfb239c8e4
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2026-04-23 11:49:25 -0700

    feature: add footer and layout container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,14 @@
 	import { onMount } from 'svelte';
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/layout/Header.svelte';
+	import Footer from '$lib/layout/Footer.svelte';
 	import BackButton from '$lib/buttons/BackButton.svelte';
 	import { page } from '$app/state';
 	import { themeStore } from '$lib/stores/theme.svelte';
 
 	let { children } = $props();
 
-	// Do not show back button on home page
+	// Do not show the back button on the home page
 	const currentUrl = $derived(page.url.pathname);
 	const renderBackButtonCond = $derived(currentUrl !== '/');
 
@@ -23,10 +24,16 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<Header label="My Doryoku Log" />
+<div class="layout-container">
+	<Header label="My Doryoku Log" />
 
-{#if renderBackButtonCond}
-	<BackButton label="Home" href="/" />
-{/if}
+	<main class="layout-content">
+		{#if renderBackButtonCond}
+			<BackButton label="Home" href="/" />
+		{/if}
 
-{@render children()}
+		{@render children()}
+	</main>
+
+	<Footer />
+</div>
