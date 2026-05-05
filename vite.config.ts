708d4ae0a3e704af17f98816b9cae73109b3bcde
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2026-05-04 23:18:07 -0700

    test: planned unit testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit(), devtoolsJson()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
 	server: {
 		proxy: {
 			'/api': {
